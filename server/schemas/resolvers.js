c6c9f4b98f05f74bbb6b907f742c7d8f8803bbe1
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-05-12 23:36:12 -0500

    added auth middleware

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,6 +5,14 @@ const { signToken } = require('../utils/auth');
 const resolvers = {
 
   Query: {
+    me: async (parent, args) => {
+      const userData = await User.findOne({})
+        .select('-__v -password')
+        .populate('thoughts')
+        .populate('friends');
+  
+      return userData;
+    },
     thoughts: async (parent, { username }) => {
       const params = username ? { username } : {};
       return Thought.find(params).sort({ createdAt: -1 });
