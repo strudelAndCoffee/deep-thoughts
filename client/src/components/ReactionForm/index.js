36fc5f45f05ab55f71aceaf000739a8c69522ac3
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-05-20 16:07:01 -0500

    added char count to reaction form

---
+++ client/src/components/ReactionForm/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+const ReactionForm = ({ thoughtId }) => {
+    const [reactionBody, setBody] = useState('');
+    const [characterCount, setCharacterCount] = useState(0);
+    
+    const handleChange = event => {
+      if (event.target.value.length <= 280) {
+        setBody(event.target.value);
+        setCharacterCount(event.target.value.length);
+      }
+    };
+    
+    const handleFormSubmit = async event => {
+      event.preventDefault();
+      setBody('');
+      setCharacterCount(0);
+    };
+
+  return (
+    <div>
+      <p className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}>
+        Character Count: {characterCount}/280
+        {error && <span className="ml-2">Something went wrong...</span>}
+      </p>
+      <form className="flex-row justify-center justify-space-between-md align-stretch">
+        <textarea
+          placeholder="Leave a reaction to this thought..."
+          className="form-input col-12 col-md-9"
+        ></textarea>
+
+        <button className="btn col-12 col-md-3" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ReactionForm;
